23fe8371b8044e8cd39ef4626cedbaae0afae364
allenorton <norton.allene@gmail.com>
2020-08-27 10:10:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+
+
+
+
+function submitData(username, emailAddress) {
+    let userInfo = { name: username, email: emailAddress }
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userInfo)
+    }
+    return fetch('http://localhost:3000/users', configObj)
+    .then( function(res){
+        return res.json()
+    })
+    .then(function (obj) {
+        console.log(obj)
+        document.body.innerHTML = obj["id"]
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message
+    })
+}
